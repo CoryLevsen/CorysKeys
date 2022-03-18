d3c68707e814083d73bd244e2a16e8d977ba95ed
Cory Levsen <corylevsen@hotmail.com>
2022-03-18 11:27:22 -0600

    Broken firebase auth

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {useNavigate} from "react-router-dom";
+
+import {signInWithEmailAndPassword} from "firebase/auth"
+import {auth} from './../libs/firebase'
+
+import {ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function LoginComponent() {
 
     let navigate = useNavigate();
 
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
     return (
+        
         <div className="login-page-container">
             <div className="form-column">
-            <h1>Cory's Keys</h1>
+                <h1>Cory's Keys</h1>
                 <div className="card" data-step>
                     
                     <h2>Welcome!</h2>
@@ -22,11 +33,20 @@ export default function LoginComponent() {
                         <input required type="password" name="password" id="password" />
                     </div>
                     <button type="button" className="button-login-style" onClick={() => {
-                        navigate("/dashboard");
+                        signInWithEmailAndPassword(auth, email, password)
+                        .then(userCredential=>{
+                            //navigate("/dashboard");
+
+                            console.log(userCredential)
+                        })
+                        .catch(error=>{
+                            console.log(error)
+                        })
                     }}>Login
                     </button>
 
                     <a href='/'>Don't have an account? Create one!</a>
+                    <ToastContainer />
                 </div>
             </div>
             <div className="info-column">
